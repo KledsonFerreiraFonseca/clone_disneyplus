bf8b660f096d0c56abe49aa4836e922349d3c945
Kledson Ferreira Fonseca <kledferr@gmail.com>
2025-12-03 14:52:20 -0300

    criando  o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+/*importar pacotes*/
+const gulp = require('gulp');/*gulp*/
+const sass = require('gulp-sass')(require('sass'));/*sass*/
+const imagemin = require('gulp-imagemin'); /*plugin de images*/
 
+/*função styles*/
 function styles(){
     return gulp.src('./src/styles/*.scss') /*arquivos fontes*/
         .pipe(sass({ style: 'compressed'})) /*encadiar a compilação para os arquivos serem comprimidos*/
         .pipe(gulp.dest('./dist/css'));/*vamos enviar estes arquivos CSS já comprimidos para uma pasta*/
 }
 
-exports.default = styles;
+/*função images*/
+function images(){
+    return gulp.src('./src/images/**/*') /*qualquer pastaque esteja dentro de "images"*/
+        .pipe(imagemin()) /*execução do imagemin*/
+        .pipe(gulp.dest('./dist/images'));/*pasta de destino para comprimir*/
+}
+
+/*exportar funções*/
+exports.default = gulp.parallel(styles, images);/*para executar o sistema em paralelo*/
+
 exports.watch = function() {/*função para sempre atualizar as alterações do projeto*/
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
